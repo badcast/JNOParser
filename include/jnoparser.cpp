e1e02981c0d47e68a7973ac2d6aba60e0ae085d6
badcast <lmecomposer@gmail.com>
2022-11-03 15:22:56 +0600

    fixing ...

--- include/jnoparser.cpp
+++ include/jnoparser.cpp
@@ -345,42 +345,43 @@ method jno_object_parser::jno_object_parser() {}
 
 method jno_object_parser::~jno_object_parser() {}
 
-method int jno_avail_only(jno_evaluated& eval, const char* jno_source, int length, int depth) {
-    int x, y;
+method int jno_avail_only(jno_evaluated& eval, const char* source, int length, int depth) {
+    int x, y, z;
     JNOType valueType;
     JNOType current_block_type;
+    const char* pointer = source;
 
     // base step
     if (!length) return 0;
 
-    for (x ^= x; x < length && jno_source[x];) {
+    for (x ^= x; x < length && *pointer;) {
         // has comment line
-        y = x += jno_skip_comment(jno_source + x, length - x);
-        if (depth > 0 && jno_source[x] == jno_syntax.jno_array_segments[1]) break;
-        x += jno_skip(jno_source + x, length - x);
+        y = x += jno_skip_comment(pointer + x, length - x);
+        if (depth > 0 && pointer[x] == jno_syntax.jno_array_segments[1]) break;
+        x += jno_skip(pointer + x, length - x);
         // check property name
-        if (!jno_is_property(jno_source + y, x - y)) throw std::bad_exception();
+        if (!jno_is_property(pointer + y, x - y)) throw std::bad_exception();
 
         // property name
         // prototype_node.propertyName.append(jno_source + y, static_cast<size_t>(x - y));
 
         //  has comment line
-        x += jno_skip_comment(jno_source + x, length - x);
-        x += jno_trim(jno_source + x, length - x);  // trim string
+        x += jno_skip_comment(pointer + x, length - x);
+        x += jno_trim(pointer + x, length - x);  // trim string
         // is block or array
-        if (jno_source[x] == *jno_syntax.jno_array_segments) {
+        if (pointer[x] == *jno_syntax.jno_array_segments) {
             ++depth;
-            if (jno_is_array(jno_source + x, y, length - x)) {
+            if (jno_is_array(pointer + x, y, length - x)) {
                 y += x++;
                 current_block_type = JNOType::Unknown;
-                //prototype_node.flags = Node_ArrayFlag;
+                // prototype_node.flags = Node_ArrayFlag;
                 for (; x < y;) {
-                    x += jno_skip_comment(jno_source + x, y - x);
+                    x += z = jno_skip_comment(pointer + x, y - x);
                     // next index
-                    if (jno_source[x] == jno_syntax.jno_obstacle)
+                    if (pointer[x] == jno_syntax.jno_obstacle)
                         ++x;
                     else {
-                        x += jno_get_format(jno_source + x, nullptr, valueType);
+                        x += z = jno_get_format(pointer + x, nullptr, valueType);
 
                         // JNO current value Type
 
@@ -413,22 +414,21 @@ method int jno_avail_only(jno_evaluated& eval, const char* jno_source, int lengt
                             }
                         }
                     }
-                    x += jno_skip_comment(jno_source + x, y - x);
+                    x += jno_skip_comment(pointer + x, y - x);
                 }
-
 
             } else {  // get the next node
                 ++x;
-                x += jno_avail_only(eval, jno_source + x, length - x, depth);
-                x += jno_skip_comment(jno_source + x, length - x);
+                x += jno_avail_only(eval, pointer + x, length - x, depth);
+                x += jno_skip_comment(pointer + x, length - x);
             }
             --depth;
-            if (jno_source[x] != jno_syntax.jno_array_segments[1]) {
+            if (pointer[x] != jno_syntax.jno_array_segments[1]) {
                 throw std::bad_exception();
             }
             ++x;
         } else {  // get also value
-            x += jno_get_format(jno_source + x, nullptr, valueType);
+            x += jno_get_format(pointer + x, nullptr, valueType);
 
             switch (valueType) {
                 case JNOType::JNOString:
@@ -450,7 +450,7 @@ method int jno_avail_only(jno_evaluated& eval, const char* jno_source, int lengt
             // prototype_node.flags = Node_ValueFlag | valueType << 2;
         }
 
-        //entry.insert(std::make_pair(y = jno_string_to_hash(prototype_node.propertyName.c_str()), prototype_node));
+        // entry.insert(std::make_pair(y = jno_string_to_hash(prototype_node.propertyName.c_str()), prototype_node));
 
         /*
         #if defined(QDEBUG) || defined(DEBUG)
@@ -461,7 +461,7 @@ method int jno_avail_only(jno_evaluated& eval, const char* jno_source, int lengt
                 _dbgLastNode = &_curIter->second;
         #endif
         */
-        x += jno_skip_comment(jno_source + x, length - x);
+        x += jno_skip_comment(pointer + x, length - x);
     }
 
     return x;
@@ -653,11 +653,10 @@ method void jno_object_parser::deserialize(const jstring& source) { deserialize(
 method void jno_object_parser::deserialize(const char* source, int len) {
     // FIXME: CLEAR FUNCTION IS UPGRADE
     entry.clear();  // clears alls
-    jno_evaluated eval={};
+    jno_evaluated eval = {};
     jno_avail_only(eval, source, len, 0);
 
     auto by = eval.calcBytes();
-
 }
 method jstring jno_object_parser::serialize() {
     jstring data;
@@ -696,7 +695,7 @@ method jno_object_node* jno_object_parser::find_node(const jstring& nodePath) {
         iter = entry->find(hash);
         if (iter != end(*entry)) {
             if (iter->second.isStruct())
-                entry = nullptr;//decltype(entry)(iter->second._bits);
+                entry = nullptr;  // decltype(entry)(iter->second._bits);
             else {
                 if (r == nodePath.length()) node = &iter->second;  // get the next section
                 break;
